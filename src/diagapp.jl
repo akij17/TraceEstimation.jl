a772b7d0aced6ea5f58b48c882ac282ada954da1
Akshay Jain <lucax_aks@hotmail.com>
2019-03-22 14:33:06 +0530

    Experimental progress for estimators

--- src/diagapp.jl
+++ src/diagapp.jl
@@ -1,58 +1,67 @@
 # Diagonal Approximation for trace of inverse of matrix
+# Based on moment prediction by Aitken's Process
+# As described in Extrapolation Methods for Estimating Trace of the Matrix Inverse by P. Fika
 
 export diagapp
 
 using LinearAlgebra
 
-# Extrapolation for c-1 and calculating value of v0
-function v0(A)
+struct HutchWorkspace
+    A::AbstractArray{Float64, 2}
+    x::AbstractArray{Float64, 1}
+    y::AbstractArray{Float64, 1}
+    N::Int64
+    skipverify::Bool
+end
+
+"""
+    HutchWorkspace(A)
+
+# Arguments
+ - `A` : Symmetric Hermitian Matrix 
+ - `N` : Number of iterations (Default: 30)
+ - `skipverify` : If false, it will check isposdef(A) (Default: false)
+"""
+function HutchWorkspace(A; N = 30, skipverify = false)
     x = rand(-1:2:1, size(A)[1])
+    y = similar(x)
+    return HutchWorkspace(A, x, y, N, skipverify)
+end
+
+function gfun(w)
     
-    c0 = dot(x, x)
-    Ax = A * x;
-    c1 = dot(x, Ax)
-    AAx = A * Ax
-    c2 = dot(x, AAx)
-    AAAx = A * AAx
-    c3 = dot(x, AAAx)
+    # Create new random values for x
+    copyto!(w.x, rand(-1:2:1, size(w.A)[1]))
 
-    p = (c0 * c2)/(c1^2)
-    
-    v0 = log10((c1^2)/(c0 * c2)) / log10((c1 * c3)/(c2^2))
+    # Find c(r) = x' * A^r * x = dot(x', A^r * x)
+    c0 = dot(w.x, w.x)
+    mul!(w.y, w.A, w.x)
+    c1 = dot(w.y, w.x)
+    c2 = dot(w.y, w.y)
+    mul!(w.x, w.A, w.y)
+    c3 = dot(w.y, w.x)
+
+    #g1 = c0 - (((c1 - c0)^2) / (c2 - c1))
+    g2 = c1 - (((c2 - c0) * (c2 - c1)) / (c3 - c2))
+
+    #g = (g1 + g2)/2
 end
-# Calculating the Approximation for ith value of inverse diagonal
-function dfun(A, i, v)
-    isum = 0.0
-    s = size(A)[1]
-    for k in 1:s
-        isum = isum + A[k, i]^2
+
+function diagapp(w::HutchWorkspace)
+    if w.skipverify == true || isposdef(w.A)
+        tr = 0.0
+        sum = 0.0
+        for i in 1:w.N
+            sum = sum + gfun(w)
+        end
+        tr = sum/w.N
     end
-    #v = v0(A)
-    p = isum / (A[i, i]^2)
-    d = 1 / (p^v * A[i, i])
 end
 
-# Calculating Diagonal Approximation for Inverse of Matrix
-function diagapp(A)
-    tr = 0.0
-    s = size(A)[1]
-    v = v0(A)
-    for i in 1:s
-       tr = tr + dfun(A, i, v)
-    end
-    tr 
-end
+A = rand(5000, 5000)
 
-#= TESTS
-#Creating a random SPD matrix
-n = 10000
-A = rand(n, n)
-A = A + A' + n*I
-#A = [2 -1 0; -1 2 -1; 0 -1 2]
+A = A + A' + (5000 * I)
 
-#Running hutchinson
-@show @time diagapp(A)
-
-#Actual value
-@show @time tr(inv(A))
-=#
+w = HutchWorkspace(A, skipverify = true)
+@time @show diagapp(w)
+@time @show tr(inv(A))
--- src/hutchinson.jl
+++ src/hutchinson.jl
@@ -2,62 +2,93 @@
 # Based on Hutchinson's Stochastic Estimation Proof
 # Values from Extrapolation Methods for Estimating Trace of the Matrix Inverse by P. Fika
 
-export hutch
+export hutch, hutch!, HutchWorkspace
 
 using LinearAlgebra
+using CuArrays
+
+struct HutchWorkspace
+    A::AbstractArray{Float64, 2}
+    x::AbstractArray{Float64, 1}
+    y::AbstractArray{Float64, 1}
+    N::Int64
+    skipverify::Bool
+end
+
+"""
+    HutchWorkspace(A)
+
+# Arguments
+ - `A` : Symmetric Hermitian Matrix 
+ - `N` : Number of iterations (Default: 30)
+ - `skipverify` : If false, it will check isposdef(A) (Default: false)
+"""
+function HutchWorkspace(A; N = 30, skipverify = false)
+    x = rand(-1:2:1, size(A)[1])
+    y = similar(x)
+    return HutchWorkspace(A, x, y, N, skipverify)
+end
+
 
 # Calculating the moments and extrapolating c-1 
-function ev(A)
-    # Create a vector with +-1 values with 0.5 probability 
-    x = rand(-1:2:1, size(A)[1])
-    
+function ev(w::HutchWorkspace)
+    # Create new random values for x
+    copyto!(w.x, rand(-1:2:1, size(w.A)[1]))
+
     # Find c(r) = x' * A^r * x = dot(x', A^r * x)
-    c0 = dot(x, x)
-    Ax = A * x;
-    c1 = dot(x, Ax)
-    AAx = A * Ax
-    c2 = dot(x, AAx)
-    AAAx = A * AAx
-    c3 = dot(x, AAAx)
+    c0 = dot(w.x, w.x)
+    mul!(w.y, w.A, w.x)
+    c1 = dot(w.y, w.x)
+    c2 = dot(w.y, w.y)
+    mul!(w.x, w.A, w.y)
+    c3 = dot(w.y, w.x)
 
     # Find p (Page 175)
     p = (c0 * c2)/(c1^2)
     
-    # Find v0 (Page 179, Numerical Example)  -> Corollary 4 in [16]
+    # Find v0 (Page 179, Numerical Example) -> Corollary 4 in [16]
     v0 = log10((c1^2)/(c0 * c2)) / log10((c1 * c3)/(c2^2))
 
     # Finally find ev (Page 175)
     ev = (c0^2)/(p^v0 * c1)
 end
+"""
+    hutch(A; N = 30, skipverify = false)
 
-# Hutchinson trace estimation using extrapolation technique 
-# Page 177
-function hutch(A, N=30, skipverify = false)
-    # Verify if the matrix is Hermitian and Positive Definite 
-    # Unless third argument is true
-    if skipverify == true || isposdef == true
+Take a HutchWorkspace object as input, apply hutchinson estimation algorithm and solve for trace 
+of inverse of the matrix. (in-place version also available)
+"""
+# Hutchinson trace estimation using extrapolation technique (Page 177)
+function hutch(A; N=30, skipverify = false)
+    w = HutchWorkspace(A, N = N, skipverify = skipverify)
+    return hutch!(w)
+end
+"""
+    hutch!(w::HutchWorkspace)
+
+Take a HutchWorkspace object as input, apply hutchinson estimation algorithm and solve for trace 
+of inverse of the matrix. (in-place version of hutch)
+"""
+function hutch!(w::HutchWorkspace)
+    if w.skipverify == true || isposdef(w.A)
         tr = 0.0
         sum = 0.0
-        # (Page 179)
-        for i in 1:N
-            sum = sum + ev(A)
+        for i in 1:w.N
+            sum = sum + ev(w)
         end
-        tr = sum/N
+        tr = sum/w.N
     end
 end
 
-#= TESTS
-
-#Creating a random SPD matrix
-n = 5000
-A = rand(n, n)
-A = A + A' + n*I
-#A = [2 -1 0; -1 2 -1; 0 -1 2]
-
-#Running hutchinson
-@show @time hutch(A)
-
-#Actual value
-@show @time tr(inv(A))
-
+#=
+A = rand(5000, 5000)
+for i in 1:5000
+    for j in 1:5000
+        A[i, j] = exp(-2 * abs(i - j))
+    end
+end
+w = HutchWorkspace(A, skipverify = false)
+@time @show hutch(A)
+@time @show hutch!(w)
+@time @show tr(inv(A))
 =#
